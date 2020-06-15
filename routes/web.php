819c14c0b85365f11513d582f9446a89bceb65ea
imane <imaneabououakil@gmail.com>
2020-06-15 17:28:45 +0200

    Template laravel avec BD

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/courses', function () {
+    return view('courses');
+});
+
+Route::get('/elements', function () {
+    return view('elements');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
